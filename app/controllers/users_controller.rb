cd1f055972beef962202b3933dcfd3a4cef71d24
hoangquangchinh <hoang.quang.chinh@sun-asterisk.com>
2020-08-04 10:24:44 +0700

    chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
+      log_in @user
       flash[:success] = t "users.new.flash_success"
       redirect_to @user
     else
